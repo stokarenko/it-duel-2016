455c3fee43a1a8b23e318e9378e372249ec2a636
Dmitriy Kiriyenko <dmitriy.kiriyenko@gmail.com>
2015-11-30 15:01:37 +0300

    Update net_solver.rb

--- net_solver.rb
+++ net_solver.rb
@@ -3,7 +3,8 @@
 require 'httparty'
 require_relative 'tetris'
 
-SIZE = ARGV.first.to_i
+SIZE = ARGV[0].to_i
+TIMEOUT = ARGV[1].to_i
 
 class Tetramino
   include HTTParty
@@ -15,32 +16,26 @@ class Tetramino
   end
 
   def fetch
-    res = self.class.get("/puzzle", query: make_query(size: SIZE)).parsed_response
-    p res
-    res
+    self.class.get("/puzzle", query: make_query(size: SIZE)).parsed_response
   end
 
   def solve(task)
-    Tetris.new(id: task["id"], size: SIZE, signature: task["signature"]).solve(timeout: 6)
+    Tetris.new(id: task["id"], size: SIZE, signature: task["signature"]).solve(timeout: TIMEOUT)
   end
 
   def send(task, result)
-    command =  "curl --request POST --data \"token=0c07cedd-397b-491a-9173-370a1969dd97&id=#{task["id"]}&solution=#{result.to_json.inspect}\" http://tetro.andy128k.net/api/puzzle"
-    puts command
-    `#{command}`
+    self.class.get("/puzzle", query: make_query(id: task[:id], solution: result.to_json.inspect)).parsed_response
   end
 
   def work!
     task = fetch
     res = solve(task)
-    p task: task, solve: res
     send(task, res) if res
   end
 
 end
 
 client = Tetramino.new
-# p client.work!
 
 while true
   p client.work!
