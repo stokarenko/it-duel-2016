41d45f7861a4de012d22bc43430de1bf9b7629c8
Sergey Tokarenko <st@anahoret.com>
2015-12-06 10:25:09 +0300

    3 times faster connectivity, but still not perfect

--- lib/tetris/connectivity.rb
+++ lib/tetris/connectivity.rb
@@ -7,18 +7,32 @@ module Tetris
         empty_points = Array.new(size**2, false)
         empty_points[cell] = true
 
+        empty_cells_count = 1
         queue = [cell]
+
         while(center_cell = queue.shift)
-          [center_cell-1, center_cell+1, center_cell-size, center_cell+size].each do |nearby_cell|
+          nearby_cells(size, center_cell) do |nearby_cell|
             if !empty_points[nearby_cell] && board[nearby_cell] == 0
               empty_points[nearby_cell] = true
               queue.push(nearby_cell)
+              empty_cells_count += 1
             end
           end
         end
 
-        empty_points.count{|i| i} % 4 == 0
+        empty_cells_count % 4 == 0
       end
+
+      private
+
+      def nearby_cells(size, center_cell)
+        # return enum_for(:nearby_cells, size, center_cell) unless block_given?
+        yield(center_cell - 1)
+        yield(center_cell + 1)
+        yield(center_cell - size)
+        yield(center_cell + size)
+      end
+
     end
   end
 end
